618a9c9687e5abdd7fd894c6b43c35b1d8bc758d
suchenghao10349 <suchenghao10349@ipalfish.com>
2021-07-11 18:24:33 +0800

    feat: 数据结构入门 day：1,存在重复元素

---
+++ datastructstart/oneday/two_seventeen/217.go
@@ -0,0 +1,13 @@
+package two_seventeen
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]bool)
+	for _, num := range nums {
+		if m[num] {
+			return true
+		}
+		m[num] = true
+	}
+
+	return false
+}
