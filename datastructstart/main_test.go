89c8a4b286e4841445d88f4d9f623ea04dd970f5
suchenghao10349 <suchenghao10349@ipalfish.com>
2021-07-12 23:36:56 +0800

    feat: day2：合并两个有序数组

---
+++ datastructstart/main_test.go
@@ -0,0 +1,10 @@
+package datastructstart
+
+import (
+	"leetcode-go/datastructstart/twoday/eightyeight"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	eightyeight.Merge([]int{2,0},1,[]int{1},1)
+}
