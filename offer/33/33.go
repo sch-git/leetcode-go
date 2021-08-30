90180c3ac01fbac5c55985f399d5a4a556e12e33
suchenghao10349 <suchenghao10349@ipalfish.com>
2021-08-30 23:47:03 +0800

    feat: 剑指offer33: 二叉搜索树的后序遍历序列

---
+++ offer/33/33.go
@@ -0,0 +1,31 @@
+package _33
+
+import "math"
+
+// middle
+// 二叉树
+
+func verifyPostorder(postorder []int) bool {
+	stack := make([]int, 0)
+	root := math.MaxInt64
+	for i := len(postorder) - 1; i >= 0; i-- {
+		stackLen := len(stack)
+		if root < postorder[i] {
+			return false
+		}
+		if stackLen == 0 || (stackLen > 0 && postorder[i] > stack[stackLen-1]) {
+			stack = append(stack, postorder[i])
+			continue
+		}
+		for stackLen > 0 && postorder[i] < stack[stackLen-1] {
+			if root < stack[stackLen-1] {
+				return false
+			}
+			root = stack[stackLen-1]
+			stack = stack[:stackLen-1]
+			stackLen = len(stack)
+		}
+		stack = append(stack, postorder[i])
+	}
+	return true
+}
