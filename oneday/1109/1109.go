8da221b60174372451e6cc59ccd56d917a71639e
suchenghao10349 <suchenghao10349@ipalfish.com>
2021-08-31 16:56:46 +0800

    feat: 每日一题 航班预订统计

--- oneday/1109/1109.go
+++ oneday/1109/1109.go
@@ -2,15 +2,12 @@ package _1109
 
 // middle
 // 滑动窗口
+// feat: 差分
 
 func corpFlightBookings(bookings [][]int, n int) []int {
-	ans := make([]int, 0)
-	for len(ans) < n {
-		ans = append(ans, 0)
-	}
+	ans := make([]int, n)
 
 	for _, booking := range bookings {
-
 		for start := booking[0] - 1; start < booking[1]; start++ {
 			ans[start] += booking[2]
 		}
--- oneday/1109/1109_test.go
+++ oneday/1109/1109_test.go
@@ -1,6 +1,8 @@
 package _1109
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestName(t *testing.T) {
 	corpFlightBookings([][]int{{1, 2, 10}, {2, 3, 20}, {2, 5, 25}}, 5)
